3ea4268078ae179c3fe60a53da1eaa4258e39185
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2019-01-03 22:40:49 -0500

    Connecting view with model through view controller

--- mvc-ifyme-capn/Controller/Controller.swift
+++ mvc-ifyme-capn/Controller/Controller.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class Controller: UIViewController {
 
+    @IBOutlet weak var iphoneNameLabel: UILabel!
+    @IBOutlet weak var iphoneColorLabel: UILabel!
+    @IBOutlet weak var iphonePriceLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let appleProduct = AppleProduct(name: "iPhone X", color: "Space Gray", price: 999.99)
+        
+        iphoneNameLabel.text = appleProduct.name
+        iphoneColorLabel.text = "in \(appleProduct.color)"
+        iphonePriceLabel.text = "$\(appleProduct.price)"
     }
 
 
