931249da4c369b6acbfb1d773d9b163d7ad031fd
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2019-01-03 22:05:50 -0500

    Initial commit

---
+++ mvc-ifyme-capn/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mvc-ifyme-capn
+//
+//  Created by Ricardo Herrera Petit on 1/3/19.
+//  Copyright © 2019 Ricardo Herrera Petit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
